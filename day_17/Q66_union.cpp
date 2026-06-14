c7ff04d7fa1116f59d3a128ebaa0d650590417b5
nocapninja <ig6612374@gmail.com>
2026-06-14 12:12:45 +0530

    Add files via upload

---
+++ day_17/Q66_union.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void input(vector<int> &a)
+{
+    for (int i = 0; i < a.size(); i++)
+    {
+        cin >> a[i];
+    }
+}
+void display(vector<int> &a)
+{
+    for (int i = 0; i < a.size(); i++)
+    {
+        cout << a[i] << " ";
+    }
+}
+int main()
+{
+    int n1, n2;
+    cout << "Enter size of array 1 : ";
+    cin >> n1;
+    vector<int> v1(n1);
+    cout << "Enter elements of array 1 : " << endl;
+    input(v1);
+    cout << "Enter size of array 2 : ";
+    cin >> n2;
+    vector<int> v2(n2);
+    cout << "Enter elements of array 2 : " << endl;
+    input(v2);
+    int j = 0;
+    while (j < n2)
+    {
+        v1.push_back(v2[j]);
+        j++;
+    }
+    for (int i = 0; i < v1.size(); i++)
+    {
+        for (int j = i + 1; j < v1.size(); j++)
+        {
+            if (v1[i] == v1[j])
+            {
+                v1.erase(v1.begin() + j);
+            }
+        }
+    }
+    cout << "The union of arrays is : " << endl;
+    display(v1);
+    return 0;
+}
---
+++ day_17/Q67_intersection.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void input(vector<int> &a)
+{
+    for (int i = 0; i < a.size(); i++)
+    {
+        cin >> a[i];
+    }
+}
+void display(vector<int> &a)
+{
+    for (int i = 0; i < a.size(); i++)
+    {
+        cout << a[i] << " ";
+    }
+}
+int main()
+{
+    int n1, n2;
+    cout << "Enter size of array 1 : ";
+    cin >> n1;
+    vector<int> v1(n1);
+    cout << "Enter elements of array 1 : " << endl;
+    input(v1);
+    cout << "Enter size of array 2 : ";
+    cin >> n2;
+    vector<int> v2(n2);
+    cout << "Enter elements of array 2 : " << endl;
+    input(v2);
+    vector<int> v3;
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = 0; j < n2; j++)
+        {
+            if (v1[i] == v2[j])
+            {
+                v3.push_back(v1[i]);
+            }
+        }
+    }
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = i + 1; j < n1; j++)
+        {
+            if (v3[i] == v3[j])
+            {
+                v3.erase(v3.begin() + j);
+            }
+        }
+    }
+    cout<<"The intersection of arrays is : "<<endl;
+    display(v3);
+    return 0;
+}
